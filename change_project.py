66bbdac80f57095ffc8da2ac6e79e8ec3b4fbbea
gujin <me450722457@vip.qq.com>
2019-02-28 14:36:29 +0800

    add some def

--- change_project.py
+++ change_project.py
@@ -1,12 +1,25 @@
 import json
 import requests
 
-saas_ip = 
 
 def get_token_and_tenant_uuid():  # 获取token和admin租户uuid
     endpoint = 'http://%s:35357/v3/auth/tokens' % (saas_ip)
-    payload = {"auth": {"identity": {"methods": ["password"], "password": {
-        "user": {"name": "admin", "domain": {"name": "Default"}, "password": ""}}}}}
+    payload = {
+        "auth": {
+            "identity": {
+                "methods": ["password"],
+                "password": {
+                    "user": {
+                        "name": "admin",
+                        "domain": {
+                            "name": "Default"
+                        },
+                        "password": ""
+                    }
+                }
+            }
+        }
+    }
     payload["auth"]["identity"]["password"]["user"]["password"] = saas_pass
     headers = {'Content-type': 'application/json'}
     req = requests.post(endpoint, data=json.dumps(payload), headers=headers)
@@ -15,6 +28,27 @@ def get_token_and_tenant_uuid():  # 获取token和admin租户uuid
     list_tau = [token, tenant_uuid]
 
     return list_tau
+
+#通过内网ip,查找虚拟机名
+def get_instance_name(token,tenant_uuid,instance_fixed_ip):
+    endpoint = 'http://%s:8774/v2.1/%s/os-fixed-ips/%s' % (saas_ip, tenant_uuid, instance_fixed_ip)
+    headers = {'X-Auth-Token': token}
+    req = requests.get(endpoint, headers=headers)
+    res = req.json() 
+    instance_name = res["fixed_ip"]["hostname"]
+    return instance_name
+
+#通过instance_name查找instance_uuid
+def get_instance_uuid(token,tenant_uuid,instance_name):
+    endpoint = 'http://%s:8774/v2.1/%s/servers/' % (saas_ip, tenant_uuid)
+    headers = {'X-Auth-Token': token}
+    req = requests.get(endpoint, headers=headers)
+    res = req.json() 
+    instance_details = res["servers"]
+    for instance_info in instance_details:
+        if instance_info["name"] == instance_name:
+            instance_uuid == instance_info["id"]
+    return instance_uuid
 
 
 def get_flavor_name(token, tenant_uuid, instance_uuid):  # 获取flavor的name
@@ -28,8 +62,7 @@ def get_flavor_name(token, tenant_uuid, instance_uuid):  # 获取flavor的name
     #     if flavor_detail['ram'] == 2048 and flavor_detail['vcpus'] == 2:
     #         flavor_uuid = flavor_detail['id']
     #         return flavor_uuid
-    endpoint = 'http://%s:8774/v2.1/%s/servers/%s' % (saas_ip,
-                                                      tenant_uuid,
+    endpoint = 'http://%s:8774/v2.1/%s/servers/%s' % (saas_ip, tenant_uuid,
                                                       instance_uuid)
     headers = {'X-Auth-Token': token}
     req = requests.get(endpoint, headers=headers)
@@ -39,9 +72,8 @@ def get_flavor_name(token, tenant_uuid, instance_uuid):  # 获取flavor的name
 
 
 def get_volume_id(token, tenant_uuid, instance_uuid):
-    endpoint = 'http://%s:8774/v2.1/%s/servers/%s/os-volume_attachments' % (saas_ip,
-                                                                            tenant_uuid,
-                                                                            instance_uuid)
+    endpoint = 'http://%s:8774/v2.1/%s/servers/%s/os-volume_attachments' % (
+        saas_ip, tenant_uuid, instance_uuid)
     headers = {'X-Auth-Token': token}
     req = requests.get(endpoint, headers=headers)
     res = req.json()
@@ -53,9 +85,8 @@ def get_volume_id(token, tenant_uuid, instance_uuid):
 
 
 def get_instance_net_info(token, tenant_uuid, instance_uuid):
-    endpoint = 'http://%s:8774/v2.1/%s/servers/%s/os-interface' % (saas_ip,
-                                                                   tenant_uuid,
-                                                                   instance_uuid)
+    endpoint = 'http://%s:8774/v2.1/%s/servers/%s/os-interface' % (
+        saas_ip, tenant_uuid, instance_uuid)
 
     headers = {'X-Auth-Token': token}
     req = requests.get(endpoint, headers=headers)
@@ -64,5 +95,22 @@ def get_instance_net_info(token, tenant_uuid, instance_uuid):
     instance_port_mac = res["interfaceAttachments"][0]["mac_addr"]
     instance_port_id = res["interfaceAttachments"][0]["port_id"]
     instance_net_id = res["interfaceAttachments"][0]["net_id"]
-    instance_subnet_id = res["interfaceAttachments"][0]["fixed_ips"][0]["subnet_id"]
-    return instance_ip, instance_port_mac, instance_port_id, instance_net_id, instance_subnet_id
+    instance_subnet_id = res["interfaceAttachments"][0]["fixed_ips"][0][
+        "subnet_id"]
+    return instance_fixed_ip, instance_port_mac, instance_port_id, instance_net_id, instance_subnet_id
+
+
+def create_volume_snapshot(token, tenant_uuid, temp):
+    endpoint = 'http://%s:8776/v2/%s/snapshots' % (saas_ip, tenant_uuid)
+    payload = {"snapshot": {"name": "","description": "","volume_id": "","force": "true"}}
+    snapshot_ids = []
+    for i in range(len(temp)):
+        payload["snapshot"]["name"] = temp[i] + "_snapshot"
+        payload["snapshot"]["volume_id"] = temp[i]
+        payload["snapshot"]["description"] = temp[i] + "_snapshot"
+        headers = {'Content-type': 'application/json'}
+        req = requests.post(
+            endpoint, data=json.dumps(payload), headers=headers)
+        snapshot_id.append = req.json()["snapshots"][i]["id"]
+    return snapshot_ids
+
